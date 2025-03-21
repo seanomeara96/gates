b88523d21206968a5d2f2fa3dcc2a545711d7b2c
seanomeara96 <seanom96@gmail.com>
2025-03-21 15:45:34 +0000

    nameing the cart item progress

--- models/cart.go
+++ models/cart.go
@@ -56,9 +56,16 @@ func NewCartItem(cartID string, components []CartItemComponent) CartItem {
 		components[i].CartItemID = id
 	}
 
+	name := components[0].Name
+
+	if len(components) > 1 {
+		name += " and " + strconv.Itoa(len(components) - 1) + " components"
+	}
+
 	return CartItem{
 		ID:         id,
 		CartID:     cartID,
+		Name:
 		Components: components,
 		CreatedAt:  time.Now(),
 		Qty:        1,
